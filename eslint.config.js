d35c7351a35725e7e445d6e51f12b4015f7d8fab
T Chouhan <tchouhanjsm@gmail.com>
2026-09-03 20:54:24 +0530

    chore: initialize project foundation

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'no-console': 'off',
+    },
+  },
+];
